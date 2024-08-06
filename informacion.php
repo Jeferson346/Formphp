e96b142d7f0b3c0fd200a9c42703f44263154633
Jeferson346 <jefersondavidtabaresgarzon93@gmail.com>
2024-08-06 15:11:14 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,10 +1,10 @@
 <?php
 $Identificacion= $_POST ['Identificacion'];
 $NomApellidos= $_POST ['NomApellidos'];
-$Cargo= $_POST ['Cargo'];
+$cargo= $_POST ['Cargo'];
 $Salirio= $_POST ['Salario'];  
 echo "La identificacion del empleado es: ".Identificacion;
 echo "Los Nombres y Apellidos del empleado son: ".NomApellidos;
-echo "El cargo del empleado es: ".Cargo;
+echo "El cargo del empleado es: ".cargo;
 echo "El salario del empleado es: ".Salario;
 ?>
